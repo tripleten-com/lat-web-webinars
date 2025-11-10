b58e7b193e05e7915fa6c3733a9b5ecb8a6de512
Andres Munoz <muza.andres@gmail.com>
2025-11-10 12:37:02 -0600

    refactor lesson structure

--- version-09/sprint-09/project/complete/scripts/functions/toggleButtonState.js
+++
@@ -1,7 +0,0 @@
-function toggleButtonState(inputList, buttonElement) {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.disabled = false;
-  }
-};
