276ebbbb0797e13e5c79e570c5c1eadd30f1bbe4
Andres Munoz <muza.andres@gmail.com>
2025-11-10 12:17:43 -0600

    refactor lesson structure

--- version-10/sprint-07/class/complete/scripts/functions/toggleButtonState.js
+++
@@ -1,9 +0,0 @@
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("form__submit_disabled");
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove("form__submit_disabled");
-    buttonElement.disabled = false;
-  }
-};
