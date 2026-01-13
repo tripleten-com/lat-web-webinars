fa0c667b4db1f2a16fc0f1359012fda14d95fe3d
Vladislav Pavlov <vldslw@gmail.com>
2026-01-13 18:20:49 -0300

    add track10 sprint4 class1 demo

--- version-10/sprint-07/class/complete/scripts/functions/hideInputError.js
+++
@@ -1,8 +0,0 @@
-const hideInputError = (inputElement) => {
-  const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  if (formError) {
-    inputElement.classList.remove("form__input_type_error");
-    formError.classList.remove("form__input-error_active");
-    formError.textContent = "";
-  }
-};
