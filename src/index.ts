7cb1092e31930b881f8801537b2346f3a632ea2f
Vladislav Pavlov <vldslw@gmail.com>
2026-04-24 20:31:02 -0300

    add complete version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,48 @@
 import { getUserInfo, getInitialPosts } from "./fakeApi.js";
 
+// OPCIÓN 1: Usando Promesas
 
+// getUserInfo()
+//   .then((user) => console.log(user))
+//   .catch((error) => console.log(error));
+
+// OPCIÓN 2: Usando Async/Await
+// const loadUser = async () => {
+//   try {
+//     const user = await getUserInfo();
+//     console.log(user);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// loadUser();
 
 // DESAFÍO: Cargar ambos recursos (usuario y posts)
 
 // const initApp = async () => {
+//   try {
+//     const user = await getUserInfo();
+//     console.log(user);
 
+//     const posts = await getInitialPosts();
+//     console.log(posts);
+//   } catch (error) {
+//     console.log(error);
+//   }
 // };
 
 // initApp();
+
+// OPCIÓN 3: Cargar ambos recursos usando Promise.all
+const initApp = async () => {
+  try {
+    const [user, posts] = await Promise.all([getUserInfo(), getInitialPosts()]);
+    console.log(user);
+    console.log(posts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initApp();
